8835bedddff56e9bea0bbf456b8b140671159895
Clinton D'Annolfo <clinton.dannolfo@gmail.com>
2017-09-11 13:44:03 -0400

    Changing the comments page to at least be something

--- src/pages/front.js
+++ src/pages/front.js
@@ -3,7 +3,6 @@ import React from 'react';
 import Main from '../layouts/Main';
 import NewsFeed from '../components/NewsFeed';
 
-import data from '../data/SampleData';
 
 const HomePage = () => (
   <Main>
--- src/pages/item.js
+++ src/pages/item.js
@@ -1,7 +1,16 @@
 import React from 'react';
 
+import Main from '../layouts/Main';
+import NewsFeed from '../components/NewsFeed';
+
+import data from '../data/SampleData';
+
 const HomePage = () => (
-  <h1>Home</h1>
+  <Main>
+    <NewsFeed
+      newsItems={data.newsItems /*this.props.newsItems*/}
+    />
+  </Main>
 );
 
 export default HomePage;
